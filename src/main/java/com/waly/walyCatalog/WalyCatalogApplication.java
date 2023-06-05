427a1ef12eebd6a6a5d039867c3cfac74e2997fd
walyson <walysonmoises1342@gmail.com>
2023-06-04 23:05:31 -0300

    create project

---
+++ src/main/java/com/waly/walyCatalog/WalyCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.waly.walyCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalyCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalyCatalogApplication.class, args);
+	}
+
+}
